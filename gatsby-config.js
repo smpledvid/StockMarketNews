13423749fa450d2e666fbc0c1d9a3d17a64c2a19
David Liang <davidd.j.liang@gmail.com>
2019-08-19 19:41:00 -0700

    Modified packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,6 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-sass`,
-    `gatsby-plugin-material-ui`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
