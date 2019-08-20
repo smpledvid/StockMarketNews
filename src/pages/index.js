22c49c32ef8cc76a5d4ab75b52379b7f71e5fa6a
David Liang <davidd.j.liang@gmail.com>
2019-08-19 20:46:56 -0700

    Added Material UI package

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,9 @@
 import React from "react"
 //import { Link } from "gatsby"
 
+import Grid from '@material-ui/core/Grid';
+import Snackbar from '@material-ui/core/Snackbar';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
 
 import Layout from "../components/layout"
 //import Image from "../components/image"
@@ -8,19 +11,19 @@ import SEO from "../components/seo"
 
 import styles from './index.module.scss'
 
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <div className={styles.titleContainer}>
       <h1>Stock Market News</h1>
-      
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          HELLO
+        </Grid>
+      </Grid>
     </div>
-    {/* <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> */}
+    {/* <Link to="/page-2/">Go to page 2</Link> */}
   </Layout>
 )
 
